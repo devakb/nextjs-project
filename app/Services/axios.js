0533558ef4b009c74fb2471a513e968b3287631f
Amit Kumar Biswas (PC) <akbdeveloper01@gmail.com>
2023-10-09 14:50:56 +0530

    update

--- app/Services/axios.js
+++ app/Services/axios.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+
+const URL = process.env.APP_URL.trimEnd("/");
+
+const APIURL = (URL ?? "http://localhost:3000") + "/api";
+
 const  instance = axios.create({
-    baseURL: process.env.APP_API_URL ?? "http://localhost:8000/api/",
+    baseURL: APIURL,
 });
 
 export default instance;
